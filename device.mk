cea0df338f4a9d188b6c54834cd92f617ce854a0
Dil3mm4 <dil3mm4.dev@gmail.com>
2018-06-10 15:30:18 +0200

    We should be good to go.

---
+++ device.mk
@@ -0,0 +1,46 @@
+#
+# Copyright (C) 2018 The LineageOS Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+#Recall proprietary hals/bins whatever blobs if they exist
+$(call inherit-product-if-exists, vendor/huawei/treble/yourmakefile.mk)
+
+#Inherit from GSI-common
+$(call inherit-product, device/phh/treble/base.mk)
+
+#Boot animation
+TARGET_SCREEN_HEIGHT := xxxx
+TARGET_SCREEN_WIDTH := xxxx
+
+#Overlays
+DEVICE_PACKAGE_OVERLAYS += \
+    $(LOCAL_PATH)/overlay 
+
+#Huawei devices don't declare fingerprint and telephony hardware feature
+PRODUCT_COPY_FILES := \
+	frameworks/native/data/etc/android.hardware.fingerprint.xml:system/etc/permissions/android.hardware.fingerprint.xml \
+	frameworks/native/data/etc/android.hardware.telephony.gsm.xml:system/etc/permissions/android.hardware.telephony.gsm.xml \
+	frameworks/native/data/etc/android.hardware.consumerir.xml:system/etc/permissions/android.hardware.consumerir.xml
+	
+#Those overrides are here because Huawei's init read properties
+#from /system/etc/prop.default, then /vendor/build.prop, then /system/build.prop
+#So we need to set our props in prop.default
+PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
+	ro.build.version.sdk=$(PLATFORM_SDK_VERSION) \
+	ro.build.version.codename=$(PLATFORM_VERSION_CODENAME) \
+	ro.build.version.all_codenames=$(PLATFORM_VERSION_ALL_CODENAMES) \
+	ro.build.version.release=$(PLATFORM_VERSION) \
+	ro.build.version.security_patch=$(PLATFORM_SECURITY_PATCH) \
+	ro.adb.secure=0
